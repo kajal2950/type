21507a352849ec6d61e47b54a05c908423404a2a
kajal2950 <payalkasayp2950@gmail.com>
2022-08-04 09:28:25 +0530

    host

--- abi/settings.py
+++ abi/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2w^i(3+i3^6c59*=gkhs-%9i2v_&*q-8e_%b4oggcy#g+v!)fz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
