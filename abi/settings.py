5156c7740fd5b47e03a85e7ce2f1d7342ddc0594
kajal2950 <payalkasayp2950@gmail.com>
2022-08-09 20:06:34 +0530

    e

--- abi/settings.py
+++ abi/settings.py
@@ -119,7 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
-STATIC_ROOT = [os.path.join(BASE_DIR,'staticfiles')]
+
+
+
+STATIC_ROOT = [os.path.join(BASE_DIR,'static')]
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
