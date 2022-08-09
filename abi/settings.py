6c2d7a6c1882befd49f96988b4685ee72d8adac9
kajal2950 <payalkasayp2950@gmail.com>
2022-08-09 20:23:17 +0530

    f

--- abi/settings.py
+++ abi/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-
+import django_heroku
+django_heroku.settigns(locals(), staticfiles=False)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
